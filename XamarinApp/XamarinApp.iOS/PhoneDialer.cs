196f5510cbf936a8e7ac9cbebf370add8503183f
Harrison Nguyen <harrison.nguyen@starixsoft.com>
2019-01-24 17:00:24 +0700

    Add project files.

---
+++ XamarinApp/XamarinApp.iOS/PhoneDialer.cs
@@ -0,0 +1,18 @@
+﻿using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using XamarinApp.iOS;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+
+namespace XamarinApp.iOS
+{
+	public class PhoneDialer : IDialer
+	{
+		public bool Dial(string number)
+		{
+			return UIApplication.SharedApplication.OpenUrl(
+				new NSUrl("tel:" + number));
+		}
+	}
+}
---
+++ XamarinApp/XamarinApp/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private string translatedNumber;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            translatedNumber = PhonewordTranslator.ToNumber(phoneNumberText.Text);
+            if (!string.IsNullOrWhiteSpace(translatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
+        }
+
+        private async void OnCall(object sender, EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                    "Dial a Number",
+                    "Would you like to call " + translatedNumber + "?",
+                    "Yes",
+                    "No"))
+            {
+                var dialer = DependencyService.Get<IDialer>();
+
+                if (dialer != null)
+                {
+                    App.PhoneNumbers.Add(translatedNumber);
+                    callHistoryButton.IsEnabled = true;
+                    dialer.Dial(translatedNumber);
+                }
+            }
+        }
+
+        private async void OnCallHistory(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CallHistoryPage());
+        }
+    }
+}
